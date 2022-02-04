75255604b677eac4382c2d8cfcb29976e64fd49f
pandrew99 <ajpan@ucsd.edu>
2022-02-04 00:17:17 -0800

    success

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1, 3));
+        assertEquals(2, SkillDemo.add(1, 1));
     }
 }
